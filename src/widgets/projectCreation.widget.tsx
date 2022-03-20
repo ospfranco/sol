8e62d03426cfc94b3013a51fca153ccf91bebcf9
Oscar Franco <ospfranco@protonmail.com>
2022-03-20 18:28:34 +0100

    Fix text input margins on project creation

--- src/widgets/projectCreation.widget.tsx
+++ src/widgets/projectCreation.widget.tsx
@@ -17,15 +17,18 @@ export const ProjectCreationWidget: FC<IProps> = observer(({style}) => {
     <View style={tw.style(`flex-1 items-center p-6 justify-center`, style)}>
       <View style={tw`w-92`}>
         <Text style={tw`font-medium`}>Create Tracking Project</Text>
-        <TextInput
-          autoFocus
-          // @ts-ignore
-          // enableFocusRing={false}
-          value={store.ui.tempProjectName}
-          onChangeText={store.ui.setTempProjectName}
-          placeholder="Project name"
-          style={tw`w-full rounded border border-gray-500 bg-transparent px-2 py-2 mt-4`}
-        />
+        <View
+          style={tw`w-full rounded border border-gray-500 dark:border-gray-700 bg-transparent px-2 py-2 mt-4`}>
+          <TextInput
+            autoFocus
+            // @ts-ignore
+            enableFocusRing={false}
+            value={store.ui.tempProjectName}
+            onChangeText={store.ui.setTempProjectName}
+            placeholder="Project name"
+            style={tw`w-full`}
+          />
+        </View>
       </View>
     </View>
   )
