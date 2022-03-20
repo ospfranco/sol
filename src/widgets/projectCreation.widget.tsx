e9e5eff7f8415d01ece51f289649f34ac94fa3f4
Angelk90 <angelo.k90@hotmail.it>
2022-03-20 14:04:58 +0100

    Fix textinput create project

--- src/widgets/projectCreation.widget.tsx
+++ src/widgets/projectCreation.widget.tsx
@@ -17,15 +17,17 @@ export const ProjectCreationWidget: FC<IProps> = observer(({style}) => {
     <View style={tw.style(`flex-1 items-center p-6 justify-center`, style)}>
       <View style={tw`w-92`}>
         <Text style={tw`font-medium`}>Create Tracking Project</Text>
+        <View
+            style={tw`w-full rounded border border-gray-500 dark:border-gray-700 bg-transparent px-2 py-2 mt-4`}>
         <TextInput
           autoFocus
           // @ts-ignore
-          // enableFocusRing={false}
+          enableFocusRing={false}
           value={store.ui.tempProjectName}
           onChangeText={store.ui.setTempProjectName}
           placeholder="Project name"
-          style={tw`w-full rounded border border-gray-500 bg-transparent px-2 py-2 mt-4`}
         />
+        </View>
       </View>
     </View>
   )
