dc1bd42c41390bf40475eef79c6a4a640c4acab4
David Alberto Adler <dalberto.adler@gmail.com>
2022-04-12 18:22:38 +0100

    Revert file

--- src/components/FileIcon.tsx
+++ src/components/FileIcon.tsx
@@ -1,5 +1,4 @@
 import {requireNativeComponent, ViewStyle} from 'react-native'
 
-export const FileIcon = requireNativeComponent<{url: string; style: ViewStyle}>(
-  'FileIcon',
-)
+export const FileIcon =
+  requireNativeComponent<{url: string; style: ViewStyle}>('FileIcon')
