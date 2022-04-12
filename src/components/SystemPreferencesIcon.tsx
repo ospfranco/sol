f5f789d5e5716c5a9e0882a68a9f6bdde1588094
David Alberto Adler <dalberto.adler@gmail.com>
2022-04-12 18:06:11 +0100

    Fix import

--- src/components/SystemPreferencesIcon.tsx
+++ src/components/SystemPreferencesIcon.tsx
@@ -1,3 +1,4 @@
+import React from 'react'
 import {ViewStyle} from 'react-native'
 import tw from 'tailwind'
 import {FileIcon} from './FileIcon'
@@ -6,7 +7,7 @@ export const SystemPreferencesIcon = ({style}: {style?: ViewStyle} = {}) => {
   return (
     <FileIcon
       style={style || tw`w-4 h-4`}
-      url="/System/Applications/System Preferences.app/"
+      url="/System/Applications/System Preferences.app"
     />
   )
 }
