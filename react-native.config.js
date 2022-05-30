3bf8d98cc5e11c81d91538a431021acbec0e757f
Oscar Franco <ospfranco@protonmail.com>
2022-05-30 07:00:54 +0200

    Fix compilation for macOS

--- react-native.config.js
+++ react-native.config.js
@@ -3,9 +3,21 @@ const {
   esmCustomMainFieldResolverPlugin,
 } = require('react-native-esbuild')
 
+const ASSET_EXTENSIONS = ['.jpeg', '.jpg', '.png']
+
 const commands = createEsbuildCommands(({plugins, ...rest}) => ({
   ...rest,
   plugins: plugins.concat(esmCustomMainFieldResolverPlugin()),
+  resolveExtensions: ['.tsx', '.ts', '.jsx', '.js', '.json']
+    .concat(ASSET_EXTENSIONS)
+    .map(extension => [
+      `.macos${extension}`,
+      `.ios${extension}`,
+      `.native${extension}`,
+      `.react-native${extension}`,
+      extension,
+    ])
+    .flat(),
 }))
 
 module.exports = {
