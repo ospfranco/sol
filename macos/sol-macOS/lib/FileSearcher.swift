b54f99f7b073cf6b430c8594cb8be7c8f5cbe488
Oscar Franco <ospfranco@protonmail.com>
2022-08-21 14:20:35 +0200

    Clean unused code

--- macos/sol-macOS/lib/FileSearcher.swift
+++ macos/sol-macOS/lib/FileSearcher.swift
@@ -15,20 +15,8 @@ class FileSearcher {
   }
 
   init() {
-//    print("Setting observer")
-//    nf.addObserver(forName: .NSMetadataQueryDidStartGathering, object: nil, queue: .main, using: {_ in
-//
-//      print("1 gathering")
-//    })
-//
+
     nf.addObserver(forName: .NSMetadataQueryDidUpdate, object: nil, queue: .main, using: {_ in
-//      print("2 updated: \(self.query?.results)")
-//      let results = self.query!.results.map {
-//        let item = $0 as NSMetadataItem
-//        return [
-//          "name": item.
-//        ]
-//      }
 
       var results: [Any] = []
 
@@ -46,18 +34,8 @@ class FileSearcher {
       SolEmitter.sharedInstance.dispatch(name: "onFileSearch", body: results)
     })
 
-    nf.addObserver(forName: .NSMetadataQueryDidFinishGathering, object: nil, queue: .main, using: {_ in
-//      let files = self.query!.results.map {
-//        return [
-//          name: $0.
-//        ]
-//      }
-//      print("result count", self.query!.resultCount)
-//      let results = self.query!.results
-//      SolEmitter.sharedInstance.dispatch(name: "onFileSearch", body: self.query!.results)
-    })
-
-
+//    nf.addObserver(forName: .NSMetadataQueryDidFinishGathering, object: nil, queue: .main, using: {_ in
+//    })
   }
 
 
@@ -71,7 +49,6 @@ class FileSearcher {
         self.query?.start()
       }
     }
-
   }
 }
 
