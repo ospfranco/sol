7a4af5295d0e864ec22497c95a55f84c1e0c050c
Oscar Franco <ospfranco@protonmail.com>
2022-11-27 09:40:41 +0100

    Always use file system for detecting panes

--- src/stores/systemPreferences.ts
+++ src/stores/systemPreferences.ts
@@ -3,162 +3,35 @@ import {solNative} from 'lib/SolNative'
 import {Linking} from 'react-native'
 import {ItemType} from './ui.store'
 
-const systemPanes = solNative
-  .ls('/System/Library/PreferencePanes')
-  .map(pane => ({
-    name: pane
-      .replace('.prefPane', '')
-      .split(/(?=[A-Z])/)
-      .join(' '),
-    preferenceId: `/System/Library/PreferencePanes/${pane}`,
-  }))
+const SYSTEM_PREFERENCE_PANES = '/System/Library/PreferencePanes'
+const GLOBAL_PREFERENCE_PANES = '/Library/PreferencePanes'
+const USER_PREFERENCE_PANES = `/Users/${solNative.userName()}/Library/PreferencePanes`
 
-const globalPanes = solNative.ls('/Library/PreferencePanes').map(pane => ({
+const systemPanes = solNative.ls(SYSTEM_PREFERENCE_PANES).map(pane => ({
   name: pane
     .replace('.prefPane', '')
     .split(/(?=[A-Z])/)
     .join(' '),
-  preferenceId: `/Library/PreferencePanes/${pane}`,
+  preferenceId: `${SYSTEM_PREFERENCE_PANES}/${pane}`,
 }))
 
-const userPanes = solNative
-  .ls(`/Users/${solNative.userName()}/Library/PreferencePanes`)
-  .map(pane => ({
-    name: pane
-      .replace('.prefPane', '')
-      .split(/(?=[A-Z])/)
-      .join(' '),
-    preferenceId: `/Library/PreferencePanes/${pane}`,
-  }))
+const globalPanes = solNative.ls(GLOBAL_PREFERENCE_PANES).map(pane => ({
+  name: pane
+    .replace('.prefPane', '')
+    .split(/(?=[A-Z])/)
+    .join(' '),
+  preferenceId: `${GLOBAL_PREFERENCE_PANES}/${pane}`,
+}))
 
-const venturaPanes = [...systemPanes, ...globalPanes, ...userPanes]
+const userPanes = solNative.ls(USER_PREFERENCE_PANES).map(pane => ({
+  name: pane
+    .replace('.prefPane', '')
+    .split(/(?=[A-Z])/)
+    .join(' '),
+  preferenceId: `${USER_PREFERENCE_PANES}/${pane}`,
+}))
 
-const panes = [
-  {
-    name: 'Language & Region',
-    preferenceId: 'com.apple.Localization',
-  },
-  {
-    name: 'Date & Time',
-    preferenceId: 'com.apple.preference.datetime',
-  },
-  {
-    name: 'Desktop & ScreenSaver',
-    preferenceId: 'com.apple.preference.desktopscreeneffect',
-  },
-  {
-    name: 'Display',
-    preferenceId: 'com.apple.preference.displays',
-  },
-  {
-    name: 'Dock & Menubar',
-    preferenceId: 'com.apple.preference.dock',
-  },
-  {
-    name: 'Battery',
-    preferenceId: 'com.apple.preference.battery',
-  },
-  {
-    name: 'Exposé',
-    preferenceId: 'com.apple.preference.expose',
-  },
-  {
-    name: 'General',
-    preferenceId: 'com.apple.preference.general',
-  },
-  {
-    name: 'Keyboard',
-    preferenceId: 'com.apple.preference.keyboard',
-  },
-  {
-    name: 'Mouse',
-    preferenceId: 'com.apple.preference.mouse',
-  },
-  {
-    name: 'Network',
-    preferenceId: 'com.apple.preference.network',
-  },
-  {
-    name: 'Notifications',
-    preferenceId: 'com.apple.preference.notifications',
-  },
-  {
-    name: 'Printers & Scanners',
-    preferenceId: 'com.apple.preference.printfax',
-  },
-  {
-    name: 'Screen Time',
-    preferenceId: 'com.apple.preference.screentime',
-  },
-  {
-    preferenceId: 'com.apple.preference.security',
-  },
-  {
-    preferenceId: 'com.apple.preference.sidecar',
-  },
-  {
-    preferenceId: 'com.apple.preference.sound',
-  },
-  {
-    preferenceId: 'com.apple.preference.speech',
-  },
-  {
-    preferenceId: 'com.apple.preference.spotlight',
-  },
-  {
-    name: 'Trackpad',
-    preferenceId: 'com.apple.preference.trackpad',
-  },
-  {
-    name: 'Universal Access',
-    preferenceId: 'com.apple.preference.universalaccess',
-  },
-  {
-    name: 'Apple ID',
-    preferenceId: 'com.apple.preferences.AppleIDPrefPane',
-  },
-  {
-    name: 'Bluetooth',
-    preferenceId: 'com.apple.preferences.Bluetooth',
-  },
-  {
-    name: 'Configuration Profiles',
-    preferenceId: 'com.apple.preferences.configurationprofiles',
-  },
-  {
-    preferenceId: 'com.apple.preferences.extensions',
-  },
-  {
-    name: 'Family Sharing',
-    preferenceId: 'com.apple.preferences.FamilySharingPrefPane',
-  },
-  {preferenceId: 'com.apple.preferences.icloud'},
-  {
-    name: 'Internet Accounts',
-    preferenceId: 'com.apple.preferences.internetaccounts',
-  },
-  {
-    name: 'Parental Controls',
-    preferenceId: 'com.apple.preferences.parentalcontrols',
-  },
-  {
-    preferenceId: 'com.apple.preferences.password',
-  },
-  {
-    preferenceId: 'com.apple.preferences.sharing',
-  },
-  {
-    name: 'Software Update',
-    preferenceId: 'com.apple.preferences.softwareupdate',
-  },
-  {
-    preferenceId: 'com.apple.preferences.users',
-  },
-  {
-    preferenceId: 'com.apple.preferences.wallet',
-  },
-  {preferenceId: 'com.apple.prefs.backup'},
-]
+const panes = [...systemPanes, ...globalPanes, ...userPanes]
 
 export function buildSystemPreferenceItem({
   preferenceId,
@@ -195,9 +68,5 @@ function capitalize(str: string) {
 }
 
 export function buildSystemPreferencesItems() {
-  if (solNative.OSVersion >= 13) {
-    return venturaPanes.map(buildSystemPreferenceItem)
-  } else {
-    return panes.map(buildSystemPreferenceItem)
-  }
+  return panes.map(buildSystemPreferenceItem)
 }
