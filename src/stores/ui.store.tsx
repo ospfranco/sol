5d8977de8cf8a2c18cf282c2a45fdbae6e8cb913
Oscar Franco <ospfranco@protonmail.com>
2023-03-09 20:19:33 +0100

    Add kill all apps command

--- src/stores/ui.store.tsx
+++ src/stores/ui.store.tsx
@@ -700,6 +700,29 @@ export const createUIStore = (root: IRootStore) => {
           }
       },
     },
+    {
+      icon: '☠️',
+      name: 'Kill all apps',
+      type: ItemType.CONFIGURATION,
+      callback: async () => {
+        solNative.executeAppleScript(`-- get list of open apps
+        tell application "System Events"
+          set allApps to displayed name of (every process whose background only is false) as list
+        end tell
+        
+        -- leave some apps open 
+        set exclusions to {"AppleScript Editor", "Automator", "Finder", "LaunchBar"}
+        
+        -- quit each app
+        repeat with thisApp in allApps
+          set thisApp to thisApp as text
+          if thisApp is not in exclusions then
+            tell application thisApp to quit
+          end if
+        end repeat
+        `)
+      },
+    },
     ...systemPreferenceItems,
   ]
 
