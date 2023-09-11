412f317d985298dc2da7e47ad4e001a3889401bd
Oscar Franco <ospfranco@gmail.com>
2023-09-11 12:23:31 +0200

    Remove sentryDSN

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,7 @@
 import {LogBox} from 'react-native'
 import Fuse from 'fuse.js'
 import * as Sentry from '@sentry/react-native'
+import { SentryDSN } from 'constants'
 
 LogBox.ignoreLogs(['Clipboard ', 'Component', 'Require cycle:'])
 
@@ -16,6 +17,6 @@ export const FUSE_OPTIONS: Fuse.IFuseOptions<any> = {
 
 if (!__DEV__) {
   Sentry.init({
-    dsn: 'https://51f61feb4c66446e88e4211a6aebf0b7@o386747.ingest.sentry.io/6377873',
+    dsn: SentryDSN,
   })
 }
