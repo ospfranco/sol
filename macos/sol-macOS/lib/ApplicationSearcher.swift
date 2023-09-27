99f334032ed04a3a3fb6a487f8cf3bc1c861c2c6
Oscar Franco <ospfranco@gmail.com>
2023-09-27 09:38:42 +0200

    Fix application searcher for macOS 14

--- macos/sol-macOS/lib/ApplicationSearcher.swift
+++ macos/sol-macOS/lib/ApplicationSearcher.swift
@@ -1,37 +1,71 @@
 import Cocoa
 
 class ApplicationSearcher: NSObject {
-  let fixedApps: [URL] = [
+  var fixedApps: [URL] = [
     URL(fileURLWithPath: "/System/Library/CoreServices/Finder.app"),
-    URL(fileURLWithPath: "/System/Library/CoreServices/Applications/Screen Sharing.app")
   ]
+
+  override init() {
+    super.init()
+    if #unavailable(macOS 14) {
+      fixedApps.append(
+        URL(
+          fileURLWithPath: "/System/Library/CoreServices/Applications/Screen Sharing.app"
+        )
+      )
+    }
+  }
 
   public func getAllApplications() -> [Application] {
     do {
       let runningApps = NSWorkspace.shared.runningApplications
-      let localApplicationUrl = try FileManager.default.url(for: .applicationDirectory, in: .localDomainMask, appropriateFor: nil, create: false)
+      let localApplicationUrl = try FileManager.default.url(
+        for: .applicationDirectory,
+        in: .localDomainMask,
+        appropriateFor: nil,
+        create: false
+      )
       let localApplicationUrls = getApplicationUrlsAt(localApplicationUrl)
-      let systemApplicationUrl = try FileManager.default.url(for: .applicationDirectory, in: .systemDomainMask, appropriateFor: nil, create: false)
+      let systemApplicationUrl = try FileManager.default.url(
+        for: .applicationDirectory,
+        in: .systemDomainMask,
+        appropriateFor: nil,
+        create: false
+      )
       let systemApplicationsUrls = getApplicationUrlsAt(systemApplicationUrl)
-      let userApplicationUrl = try FileManager.default.url(for: .applicationDirectory, in: .userDomainMask, appropriateFor: nil, create: false)
+      let userApplicationUrl = try FileManager.default.url(
+        for: .applicationDirectory,
+        in: .userDomainMask,
+        appropriateFor: nil,
+        create: false
+      )
       let personalApplicationUrls = getApplicationUrlsAt(userApplicationUrl)
 
       let allApplicationUrls =
-      localApplicationUrls + systemApplicationsUrls + personalApplicationUrls + fixedApps
+        localApplicationUrls + systemApplicationsUrls +
+        personalApplicationUrls +
+        fixedApps
 
       var applications = [Application]()
 
       for url in allApplicationUrls {
-        let resourceKeys: [URLResourceKey] = [.isExecutableKey, .isApplicationKey]
+        let resourceKeys: [URLResourceKey] = [
+          .isExecutableKey,
+          .isApplicationKey,
+        ]
         let resourceValues = try url.resourceValues(forKeys: Set(resourceKeys))
         if resourceValues.isExecutable ?? false {
           let name = url.deletingPathExtension().lastPathComponent
           let urlStr = url.absoluteString
           let firstRunning = runningApps.first(where: {
-            return $0.bundleURL?.absoluteString == urlStr
+            $0.bundleURL?.absoluteString == urlStr
           })
-          
-          applications.append(Application(name: name, url: urlStr, isRunning: firstRunning != nil))
+
+          applications.append(Application(
+            name: name,
+            url: urlStr,
+            isRunning: firstRunning != nil
+          ))
         }
       }
 
@@ -45,16 +79,17 @@ class ApplicationSearcher: NSObject {
   private func getApplicationUrlsAt(_ url: URL) -> [URL] {
     let fileManager = FileManager()
     do {
-      if(!url.path.contains(".app") && url.hasDirectoryPath) {
+      if !url.path.contains(".app") && url.hasDirectoryPath {
         var urls = try fileManager.contentsOfDirectory(
           at: url,
           includingPropertiesForKeys: [],
           options: [
             FileManager.DirectoryEnumerationOptions.skipsPackageDescendants,
-          ])
+          ]
+        )
 
         urls.forEach {
-          if(!$0.path.contains(".app") && $0.hasDirectoryPath) {
+          if !$0.path.contains(".app") && $0.hasDirectoryPath {
             let subUrls = getApplicationUrlsAt($0)
 
             urls.append(contentsOf: subUrls)
@@ -68,7 +103,6 @@ class ApplicationSearcher: NSObject {
     } catch {
       return []
     }
-
   }
 }
 
