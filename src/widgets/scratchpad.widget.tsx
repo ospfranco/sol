8c1ff42b6fc5e43406df953f17eb27c916ee5530
Oscar Franco <ospfranco@gmail.com>
2023-11-02 20:26:34 +0100

    Simplify scratchpad

--- src/widgets/scratchpad.widget.tsx
+++ src/widgets/scratchpad.widget.tsx
@@ -1,26 +1,14 @@
 import {useFullSize} from 'hooks/useFullSize'
 import {solNative} from 'lib/SolNative'
 import {observer} from 'mobx-react-lite'
-import React, {FC, forwardRef, useEffect, useRef} from 'react'
-import {
-  FlatList,
-  Text,
-  TextInput,
-  TextInputProps,
-  useColorScheme,
-  View,
-  ViewStyle,
-} from 'react-native'
+import React, {FC, useEffect, useRef} from 'react'
+import {FlatList, TextInput, View, ViewStyle} from 'react-native'
 import {useStore} from 'store'
 import colors from 'tailwindcss/colors'
 
 interface Props {
   style?: ViewStyle
   className?: string
-}
-
-interface WrappedInputProps extends TextInputProps {
-  focused: boolean
 }
 
 export const ScratchpadWidget: FC<Props> = observer(({style}) => {
@@ -46,25 +34,19 @@ export const ScratchpadWidget: FC<Props> = observer(({style}) => {
   }, [selectedIndex])
 
   return (
-    <View style={style} className="flex-1 p-4">
-      <View className="flex-1 rounded-lg border border-lightBorder dark:border-darkBorder p-2 bg-white dark:bg-darker">
-        <TextInput
-          autoFocus
-          value={store.ui.note}
-          onChangeText={store.ui.setNote}
-          // @ts-expect-error
-          enableFocusRing={false}
-          placeholderTextColor={colors.neutral[400]}
-          placeholder="Write something..."
-          className="flex-1"
-          multiline
-          spellCheck
-        />
-        <Text className="text-xs text-neutral-400 self-end">
-          {store.ui.note.split(' ').filter(v => v).length} Words •{' '}
-          {store.ui.note.length} Characters
-        </Text>
-      </View>
+    <View style={style} className="flex-1 px-4">
+      <TextInput
+        autoFocus
+        value={store.ui.note}
+        onChangeText={store.ui.setNote}
+        // @ts-expect-error
+        enableFocusRing={false}
+        placeholderTextColor={colors.neutral[400]}
+        placeholder="Write something..."
+        className="flex-1"
+        multiline
+        spellCheck
+      />
     </View>
   )
 })
