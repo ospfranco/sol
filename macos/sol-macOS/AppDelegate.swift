ed0272fcc24f4a5fee6f221a794f5baa0c48e678
Oscar Franco <ospfranco@gmail.com>
2025-01-21 08:01:02 +0100

    App loads

--- macos/sol-macOS/AppDelegate.swift
+++ macos/sol-macOS/AppDelegate.swift
@@ -1,7 +1,7 @@
 import EventKit
 import Foundation
-import Sparkle
 import React_RCTAppDelegate
+import Sparkle
 
 @NSApplicationMain
 @objc
@@ -9,45 +9,49 @@ class AppDelegate: RCTAppDelegate {
   private var updaterController: SPUStandardUpdaterController!
   private var mediaKeyForwarder: MediaKeyForwarder!
 
-//  override init() {
-//    updaterController = SPUStandardUpdaterController(
-//      startingUpdater: true,
-//      updaterDelegate: nil,
-//      userDriverDelegate: nil
-//    )
-//    super.init()
-//  }
-//
-//  required init?(coder: NSCoder) {
-//    fatalError("init(coder:) has not been implemented")
-//  }
+  override init() {
+    updaterController = SPUStandardUpdaterController(
+      startingUpdater: true,
+      updaterDelegate: nil,
+      userDriverDelegate: nil
+    )
+    super.init()
+  }
 
-//  override func applicationShouldHandleReopen(
-//    _: NSApplication,
-//    hasVisibleWindows _: Bool
-//  ) -> Bool {
-//    PanelManager.shared.showWindow()
-//    return true
-//  }
-  
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+
+  override func applicationShouldHandleReopen(
+    _: NSApplication,
+    hasVisibleWindows _: Bool
+  ) -> Bool {
+    PanelManager.shared.showWindow()
+    return true
+  }
+
   override func sourceURL(for bridge: RCTBridge) -> URL? {
     self.bundleURL()
   }
-  
+
   override func bundleURL() -> URL? {
-#if DEBUG
-    RCTBundleURLProvider.sharedSettings().jsBundleURL(forBundleRoot: "index")
-#else
-    Bundle.main.url(forResource: "main", withExtension: "jsbundle")
-#endif
+    #if DEBUG
+      RCTBundleURLProvider.sharedSettings().jsBundleURL(forBundleRoot: "index")
+    #else
+      Bundle.main.url(forResource: "main", withExtension: "jsbundle")
+    #endif
   }
 
   override func applicationDidFinishLaunching(_ notification: Notification) {
+    self.moduleName = "sol"
+    self.initialProps = [:]
     self.automaticallyLoadReactNativeWindow = false
     super.applicationDidFinishLaunching(notification)
-    
-    let rootViewFactoryConfiguration = RCTRootViewFactoryConfiguration(bundleURLBlock: self.bundleURL, newArchEnabled: false, turboModuleEnabled: false, bridgelessEnabled: false)
-    
+
+    let rootViewFactoryConfiguration = RCTRootViewFactoryConfiguration(
+      bundleURLBlock: self.bundleURL, newArchEnabled: false, turboModuleEnabled: true,
+      bridgelessEnabled: false)
+
     self.rootViewFactory = RCTRootViewFactory(configuration: rootViewFactoryConfiguration)
     let rootView = self.rootViewFactory.view(withModuleName: "sol")
 
