5f755c82f91856a9a185fb02e748b9e63c566e16
Oscar Franco <ospfranco@gmail.com>
2025-02-12 16:19:28 +0100

    Simplify note taker a bit

--- src/widgets/scratchpad.widget.tsx
+++ src/widgets/scratchpad.widget.tsx
@@ -1,37 +1,12 @@
-import clsx from 'clsx'
-import {GradientView} from 'components/GradientView'
-import {Key} from 'components/Key'
-import {useFullSize} from 'hooks/useFullSize'
 import {solNative} from 'lib/SolNative'
 import {observer} from 'mobx-react-lite'
-import React, {FC, useEffect} from 'react'
-import {Text, TextInput, View, ViewStyle} from 'react-native'
+import {FC, useEffect} from 'react'
+import {TextInput, View} from 'react-native'
 import {useStore} from 'store'
 import colors from 'tailwindcss/colors'
 
-interface Props {
-  style?: ViewStyle
-  className?: string
-}
-
-const BG_COLORS = {
-  SYSTEM: {
-    start: '#00000000',
-    end: '#00000000',
-  },
-  BLUE: {
-    start: '#00415277',
-    end: '#012b3677',
-  },
-  ORANGE: {
-    start: '#ff9e0077',
-    end: '#8063ff77',
-  },
-}
-
-export const ScratchpadWidget: FC<Props> = observer(({style}) => {
+export const ScratchpadWidget: FC = observer(() => {
   let store = useStore()
-  useFullSize()
 
   useEffect(() => {
     solNative.turnOffVerticalArrowsListeners()
@@ -43,11 +18,7 @@ export const ScratchpadWidget: FC<Props> = observer(({style}) => {
   }, [])
 
   return (
-    <GradientView
-      className="flex-1"
-      startColor={BG_COLORS[store.ui.scratchPadColor].start}
-      endColor={BG_COLORS[store.ui.scratchPadColor].end}
-      angle={45}>
+    <View className="flex-1">
       <TextInput
         autoFocus
         value={store.ui.note}
@@ -55,18 +26,10 @@ export const ScratchpadWidget: FC<Props> = observer(({style}) => {
         // @ts-expect-error
         enableFocusRing={false}
         placeholderTextColor={colors.neutral[400]}
-        style={{
-          fontFamily: 'Avenir',
-        }}
         placeholder="Write something..."
-        className="flex-1 p-4 mt-2"
+        className="flex-1 p-4 -mt-8"
         multiline
-        spellCheck
       />
-      <View className="flex-row gap-2 subBg justify-end px-4 py-2">
-        <Text className={clsx('text-xs darker-text')}>Switch themes</Text>
-        <Key symbol={'\u21E5'} />
-      </View>
-    </GradientView>
+    </View>
   )
 })
