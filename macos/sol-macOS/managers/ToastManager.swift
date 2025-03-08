a2ac749a1707058e04de2c086204f0efa039fbfe
Oscar Franco <ospfranco@gmail.com>
2025-03-08 16:49:31 +0100

    Fix calculation of toast on screen

--- macos/sol-macOS/managers/ToastManager.swift
+++ macos/sol-macOS/managers/ToastManager.swift
@@ -1,21 +1,23 @@
 class ToastManager {
   private var toastWindow: Toast = Toast(contentRect: .zero)
-  
+
   static public let shared = ToastManager()
-  
-  func showToast(_ text: String, variant: String, timeout: NSNumber?, image: NSImage?) {
-    guard let mainScreen = PanelManager.shared.getPreferredScreen()
+
+  func showToast(
+    _ text: String, variant: String, timeout: NSNumber?, image: NSImage?
+  ) {
+    guard let screen = PanelManager.shared.getPreferredScreen()
     else {
       return
     }
-    
+
     let variantEnum: ToastVariant =
-    switch variant {
-    case "error": .error
-    case "success": .success
-    default: .none
-    }
-    
+      switch variant {
+      case "error": .error
+      case "success": .success
+      default: .none
+      }
+
     let toastView = ToastView(
       text: text,
       variant: variantEnum,
@@ -26,55 +28,57 @@ class ToastManager {
         }
       }
     )
-    toastView.layoutSubtreeIfNeeded()  // Ensure layout is performed
-    
+    toastView.layoutSubtreeIfNeeded()
+
     let contentSize = toastView.intrinsicContentSize
-    toastView.frame = NSRect(x: 0, y: 0, width: contentSize.width, height: contentSize.height)
-    
+    let size = NSRect(
+      x: 0, y: 0, width: contentSize.width, height: contentSize.height)
+    toastView.frame = size
+
     let effectView = NSVisualEffectView(
-      frame: NSRect(x: 0, y: 0, width: contentSize.width, height: contentSize.height)
+      frame: size
     )
     effectView.autoresizingMask = [.width, .height]
     effectView.material = .hudWindow  // Or other material
     effectView.blendingMode = .behindWindow
     effectView.state = .active
-    
+
     toastWindow.contentView = effectView
     toastWindow.contentView!.addSubview(toastView)
-    
+
     // Add the effect view to the content view of the window, NOT the ToastView
     toastWindow
       .setFrame(
-        NSRect(
-          x: 0,
-          y: 0,
-          width: contentSize.width,
-          height: contentSize.height
-        ),
+        size,
         display: true
       )
-    
-    let x = mainScreen.frame.size.width / 2 - toastWindow.frame.width / 2
-    var y = mainScreen.frame.origin.y + mainScreen.frame.size.height * 0.1
-    
+    toastWindow.setContentSize(contentSize)
+
+    // 0 is calculated from the origin of the main screen, which means screen.frame.origin.x can be negative
+    let x =
+      screen.frame.origin.x
+      + (screen.frame.size.width / 2 - contentSize.width / 2)
+    var y = screen.frame.origin.y + screen.frame.size.height * 0.1
+
     if image != nil {
-      y = mainScreen.frame.origin.y + toastWindow.frame.height
+      y = screen.frame.origin.y + toastWindow.frame.height
     }
-    
+
     toastWindow.setFrameOrigin(
       NSPoint(
         x: x,
         y: y
       ))
-    
+
     toastWindow.makeKeyAndOrderFront(nil)
-    
-    let deadline = timeout != nil ? DispatchTime.now() + timeout!.doubleValue : .now() + 2
+
+    let deadline =
+      timeout != nil ? DispatchTime.now() + timeout!.doubleValue : .now() + 2
     DispatchQueue.main.asyncAfter(deadline: deadline) {
       self.dismissToast()
     }
   }
-  
+
   func dismissToast() {
     toastWindow.orderOut(nil)
   }
