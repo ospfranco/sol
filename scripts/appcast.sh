3015200208cfe1a89ad0ac65f5cd33f70b1c665b
Oscar Franco <ospfranco@gmail.com>
2025-10-05 09:14:59 +0200

    Try auto generating release notes and adding them to appcast

--- scripts/appcast.sh
+++ scripts/appcast.sh
@@ -1,18 +1,29 @@
-#!/bin/bash
+#!/bin/sh
 
 url="$1"
 version="$2"
+release_notes="$3
+
+echo "🟠 Creating release version $version"
+echo "Release Notes"
+echo "$release_notes"
 
 url_to_replace="https://raw.githubusercontent.com/ospfranco/sol/main/releases/$version.zip"
 
-# XML template
-# Read XML content from file
 xml=$(cat releases/appcast.xml)
 
-# Replace the url
 xml=${xml//"$url_to_replace"/"$url"}
 
-# Save XML content to a file
+# Add release notes to the first item (most recent version)
+# Find the first <item> and add description after the title
+if [ -n "$release_notes" ]; then
+    # Escape special characters in release notes for XML
+    escaped_notes=$(echo "$release_notes" | sed 's/&/\&amp;/g; s/</\&lt;/g; s/>/\&gt;/g; s/"/\&quot;/g; s/'"'"'/\&#39;/g')
+    
+    # Add description after the first <title> tag within an <item>
+    xml=$(echo "$xml" | sed '0,/<item>/,/<\/title>/{s|</title>|</title>\n            <description><![CDATA['"$escaped_notes"']]></description>|;}')
+fi
+
 echo "$xml" > releases/appcast.xml
 
-echo "XML file generated successfully."
+echo "🟩 Created AppCast XML correctly"
