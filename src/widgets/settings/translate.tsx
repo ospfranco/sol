9968091755bcb2787ddbaf8e006846dadd782f24
Oscar Franco <ospfranco@gmail.com>
2025-12-14 20:15:15 -0400

    Remove translation flags

--- src/widgets/settings/translate.tsx
+++ src/widgets/settings/translate.tsx
@@ -1,58 +1,64 @@
-import { Dropdown } from 'components/Dropdown'
-import { languages } from 'lib/languages'
-import { observer } from 'mobx-react-lite'
-import { Text, View } from 'react-native'
-import { useStore } from 'store'
+import { Dropdown } from "components/Dropdown";
+import { languages } from "lib/languages";
+import { observer } from "mobx-react-lite";
+import { Text, View } from "react-native";
+import { useStore } from "store";
 
 export const Translate = observer(() => {
-  const store = useStore()
+	const store = useStore();
 
-  return (
-    <View className="flex-1 py-5 px-5">
-      <View className="p-3 subBg rounded-lg border border-lightBorder dark:border-darkBorder gap-1">
-        <Text className="text-xxs text">
-          Select up to 3 languages for translation
-        </Text>
-        <View className="flex-row items-center py-2 z-20">
-          <Text className="flex-1">First language</Text>
+	return (
+		<View className="flex-1 py-5 px-5">
+			<View className="p-3 subBg rounded-lg border border-lightBorder dark:border-darkBorder gap-1">
+				<Text className="text-xxs text">
+					Select up to 3 languages for translation
+				</Text>
+				<View className="flex-row items-center py-2 z-20">
+					<Text className="flex-1">First language</Text>
 
-          <Dropdown
-            className="w-40"
-            value={store.ui.firstTranslationLanguage}
-            onValueChange={v => store.ui.setFirstTranslationLanguage(v as any)}
-            options={Object.values(languages).map(v => ({
-              label: v.name,
-              value: v.code,
-            }))}
-          />
-        </View>
-        <View className="flex-row items-center py-2 z-10">
-          <Text className="flex-1">Second language</Text>
+					<Dropdown
+						className="w-40"
+						value={store.ui.firstTranslationLanguage}
+						onValueChange={(v) =>
+							store.ui.setFirstTranslationLanguage(v as string)
+						}
+						options={Object.values(languages).map((v) => ({
+							label: v.name,
+							value: v.code,
+						}))}
+					/>
+				</View>
+				<View className="flex-row items-center py-2 z-10">
+					<Text className="flex-1">Second language</Text>
 
-          <Dropdown
-            className="w-40"
-            value={store.ui.secondTranslationLanguage}
-            onValueChange={v => store.ui.setSecondTranslationLanguage(v as any)}
-            options={Object.values(languages).map((v, index) => ({
-              label: v.name,
-              value: v.code,
-            }))}
-          />
-        </View>
-        <View className="flex-row items-center py-2">
-          <Text className="flex-1">Third language</Text>
+					<Dropdown
+						className="w-40"
+						value={store.ui.secondTranslationLanguage}
+						onValueChange={(v) =>
+							store.ui.setSecondTranslationLanguage(v as string)
+						}
+						options={Object.values(languages).map((v, index) => ({
+							label: v.name,
+							value: v.code,
+						}))}
+					/>
+				</View>
+				<View className="flex-row items-center py-2">
+					<Text className="flex-1">Third language</Text>
 
-          <Dropdown
-            className="w-40"
-            value={store.ui.thirdTranslationLanguage ?? ''}
-            onValueChange={v => store.ui.setThirdTranslationLanguage(v as any)}
-            options={Object.values(languages).map(v => ({
-              label: v.name,
-              value: v.code,
-            }))}
-          />
-        </View>
-      </View>
-    </View>
-  )
-})
+					<Dropdown
+						className="w-40"
+						value={store.ui.thirdTranslationLanguage ?? ""}
+						onValueChange={(v) =>
+							store.ui.setThirdTranslationLanguage(v as string)
+						}
+						options={Object.values(languages).map((v) => ({
+							label: v.name,
+							value: v.code,
+						}))}
+					/>
+				</View>
+			</View>
+		</View>
+	);
+});
--- src/widgets/translation.widget.tsx
+++ src/widgets/translation.widget.tsx
@@ -1,101 +1,99 @@
-import clsx from 'clsx'
-import {MainInput} from 'components/MainInput'
-import {useFullSize} from 'hooks/useFullSize'
-import {solNative} from 'lib/SolNative'
-import {languages} from 'lib/languages'
-import {observer} from 'mobx-react-lite'
-import {FC, useEffect} from 'react'
-import {StyleProp, Text, View, ViewStyle} from 'react-native'
-import {useStore} from 'store'
+import clsx from "clsx";
+import { MainInput } from "components/MainInput";
+import { useFullSize } from "hooks/useFullSize";
+import { solNative } from "lib/SolNative";
+import { languages } from "lib/languages";
+import { observer } from "mobx-react-lite";
+import { type FC, useEffect } from "react";
+import { type StyleProp, Text, View, type ViewStyle } from "react-native";
+import { useStore } from "store";
 
 interface Props {
-  style?: StyleProp<ViewStyle>
-  className?: string
+	style?: StyleProp<ViewStyle>;
+	className?: string;
 }
 
-export const TranslationWidget: FC<Props> = observer(({style}) => {
-  const store = useStore()
+export const TranslationWidget: FC<Props> = observer(({ style }) => {
+	const store = useStore();
 
-  useEffect(() => {
-    solNative.turnOnHorizontalArrowsListeners()
+	useEffect(() => {
+		solNative.turnOnHorizontalArrowsListeners();
 
-    return () => {
-      solNative.turnOffHorizontalArrowsListeners()
-    }
-  }, [])
+		return () => {
+			solNative.turnOffHorizontalArrowsListeners();
+		};
+	}, []);
 
-  const index = store.ui.selectedIndex
+	const index = store.ui.selectedIndex;
 
-  return (
-    <View className="flex-1" style={style}>
-      <View className="flex-row px-3">
-        <MainInput placeholder="Translate..." showBackButton />
-      </View>
+	return (
+		<View className="flex-1" style={style}>
+			<View className="flex-row px-3">
+				<MainInput placeholder="Translate..." showBackButton />
+			</View>
 
-      {!store.ui.translationResults.length && (
-        <View className="flex-1 p-3 items-center justify-center">
-          <Text className="text-xs darker-text">Translating...</Text>
-        </View>
-      )}
+			{!store.ui.translationResults.length && (
+				<View className="flex-1 p-3 items-center justify-center">
+					<Text className="text-xs darker-text">Translating...</Text>
+				</View>
+			)}
 
-      {!!store.ui.translationResults.length && (
-        <View className="flex-1">
-          <View
-            className={clsx(
-              'flex-1 p-3 m-2 flex-row items rounded overflow-hidden',
-              {
-                highlight: index === 0,
-              },
-            )}>
-            <Text className="flex-1 text" style={{fontFamily: 'Andale Mono'}}>
-              {store.ui.translationResults[0]}
-            </Text>
-            <Text className="text-8xl opacity-50 absolute top-[%20] -right-20">
-              {/* @ts-ignore */}
-              {languages[store.ui.firstTranslationLanguage]?.flag ??
-                store.ui.firstTranslationLanguage}
-            </Text>
-          </View>
+			{!!store.ui.translationResults.length && (
+				<View className="flex-1">
+					<View
+						className={clsx(
+							"flex-1 p-3 m-2 flex-row items rounded overflow-hidden",
+							{
+								highlight: index === 0,
+							},
+						)}
+					>
+						<Text className="flex-1 text" style={{ fontFamily: "Andale Mono" }}>
+							{store.ui.translationResults[0]}
+						</Text>
+						<Text className="absolute bottom-2 right-2 opacity-50">
+							{store.ui.firstTranslationLanguage}
+						</Text>
+					</View>
 
-          <View
-            className={clsx(
-              'flex-1 p-3 m-2 flex-row items rounded overflow-hidden',
-              {
-                highlight: index === 1,
-              },
-            )}>
-            <Text className="flex-1 text" style={{fontFamily: 'Andale Mono'}}>
-              {store.ui.translationResults[1]}
-            </Text>
-            <Text className="text-8xl opacity-50 absolute top-[%20] -right-20">
-              {/* @ts-ignore */}
-              {languages[store.ui.secondTranslationLanguage]?.flag ??
-                store.ui.secondTranslationLanguage}
-            </Text>
-          </View>
+					<View
+						className={clsx(
+							"flex-1 p-3 m-2 flex-row items rounded overflow-hidden",
+							{
+								highlight: index === 1,
+							},
+						)}
+					>
+						<Text className="flex-1 text" style={{ fontFamily: "Andale Mono" }}>
+							{store.ui.translationResults[1]}
+						</Text>
+						<Text className="absolute bottom-2 right-2 opacity-50">
+							{store.ui.secondTranslationLanguage}
+						</Text>
+					</View>
 
-          {!!store.ui.thirdTranslationLanguage && (
-            <View
-              className={clsx(
-                'flex-1 p-3 m-2 flex-row items rounded overflow-hidden',
-                {
-                  highlight: index === 2,
-                },
-              )}>
-              <Text
-                className="flex-1 text "
-                style={{fontFamily: 'Andale Mono'}}>
-                {store.ui.translationResults[2]}
-              </Text>
-              <Text className="text-8xl opacity-50 absolute top-[%20] -right-20">
-                {/* @ts-ignore */}
-                {languages[store.ui.thirdTranslationLanguage]?.flag ??
-                  store.ui.thirdTranslationLanguage}
-              </Text>
-            </View>
-          )}
-        </View>
-      )}
-    </View>
-  )
-})
+					{!!store.ui.thirdTranslationLanguage && (
+						<View
+							className={clsx(
+								"flex-1 p-3 m-2 flex-row items rounded overflow-hidden",
+								{
+									highlight: index === 2,
+								},
+							)}
+						>
+							<Text
+								className="flex-1 text "
+								style={{ fontFamily: "Andale Mono" }}
+							>
+								{store.ui.translationResults[2]}
+							</Text>
+							<Text className="text-sm opacity-50 absolute top-[%20] -right-20">
+								{store.ui.thirdTranslationLanguage}
+							</Text>
+						</View>
+					)}
+				</View>
+			)}
+		</View>
+	);
+});
