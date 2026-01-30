ab0c184bcdea3368d2fe9874a7bea5d0e50ee19a
Oscar Franco <ospfranco@gmail.com>
2026-01-30 11:03:32 +0100

    Fixes various clipboard issues

--- src/components/MainInput.tsx
+++ src/components/MainInput.tsx
@@ -1,75 +1,75 @@
-import { observer } from 'mobx-react-lite'
-import { DevSettings, Image, Text, TouchableOpacity, View } from 'react-native'
-import { TextInput } from 'react-native-macos'
-import { useStore } from 'store'
-import colors from 'tailwindcss/colors'
-import { Widget } from 'stores/ui.store'
-import { BackButton } from './BackButton'
-import { Assets } from 'assets'
+import { observer } from "mobx-react-lite";
+import { DevSettings, Image, Text, TouchableOpacity, View } from "react-native";
+import { TextInput } from "react-native-macos";
+import { useStore } from "store";
+import colors from "tailwindcss/colors";
+import { Widget } from "stores/ui.store";
+import { BackButton } from "./BackButton";
+import { Assets } from "assets";
 
 type Props = {
-  placeholder?: string
-  showBackButton?: boolean
-  style?: any
-  className?: string
-  hideIcon?: boolean
-}
+	placeholder?: string;
+	showBackButton?: boolean;
+	style?: any;
+	className?: string;
+	hideIcon?: boolean;
+};
 
 export const MainInput = observer<Props>(
-  ({
-    placeholder = 'Search your mac...',
-    showBackButton,
-    hideIcon,
-  }) => {
-    const store = useStore()
-    const isDarkMode = store.ui.isDarkMode
-    const reloadApp = async () => {
-      DevSettings.reload()
-    }
+	({ placeholder = "Search your mac...", showBackButton, hideIcon }) => {
+		const store = useStore();
+		const isDarkMode = store.ui.isDarkMode;
+		const reloadApp = async () => {
+			DevSettings.reload();
+		};
 
-    let leftButton = null
-    if (showBackButton) {
-      leftButton = (
-        <BackButton
-          onPress={() => {
-            store.ui.setQuery('')
-            store.ui.focusWidget(Widget.SEARCH)
-          }}
-        />
-      )
-    }
+		let leftButton = null;
+		if (showBackButton) {
+			leftButton = (
+				<BackButton
+					onPress={() => {
+						store.ui.setQuery("");
+						store.ui.focusWidget(Widget.SEARCH);
+					}}
+				/>
+			);
+		}
 
-    if (!showBackButton) {
-      leftButton = (
-        <TouchableOpacity onPress={reloadApp}>
-          <Image
-            source={isDarkMode ? Assets.logoMinimal : Assets.logoMinimalWhite}
-            style={{ width: 20, height: 20 }}
-          />
-        </TouchableOpacity>
-      )
-    }
+		if (!showBackButton) {
+			leftButton = (
+				<TouchableOpacity onPress={reloadApp}>
+					<Image
+						source={isDarkMode ? Assets.logoMinimal : Assets.logoMinimalWhite}
+						style={{ width: 20, height: 20 }}
+					/>
+				</TouchableOpacity>
+			);
+		}
 
-    if (hideIcon) {
-      leftButton = null
-    }
+		if (hideIcon) {
+			leftButton = null;
+		}
 
-    return (
-      <View className="min-h-[42px] flex-row items-center gap-2 my-1 flex-1 px-2">
-        {leftButton}
-        <TextInput
-          autoFocus
-          enableFocusRing={false}
-          value={store.ui.query}
-          onChangeText={store.ui.setQuery}
-          // @ts-ignore
-          className="text-lg flex-1"
-          cursorColor={isDarkMode ? colors.white : colors.black}
-          placeholder={placeholder}
-          placeholderTextColor={isDarkMode ? '#888' : '#888'}
-        />
-        {__DEV__ && <Text>Debug</Text>}
-      </View>
-    )
-  },
-)
+		return (
+			<View className="min-h-[42px] flex-row items-center gap-2 my-1 flex-1 px-2">
+				{leftButton}
+				<TextInput
+					autoFocus
+					enableFocusRing={false}
+					value={store.ui.query}
+					onChangeText={store.ui.setQuery}
+					// @ts-ignore
+					className="text-lg flex-1"
+					cursorColor={isDarkMode ? colors.white : colors.black}
+					placeholder={placeholder}
+					placeholderTextColor={isDarkMode ? "#888" : "#888"}
+				/>
+				{__DEV__ && (
+					<TouchableOpacity onPress={reloadApp}>
+						<Text>Debug</Text>
+					</TouchableOpacity>
+				)}
+			</View>
+		);
+	},
+);
--- src/widgets/clipboard.widget.tsx
+++ src/widgets/clipboard.widget.tsx
@@ -1,115 +1,127 @@
-import { LegendList, LegendListRef } from '@legendapp/list'
-import clsx from 'clsx'
-import { FileIcon } from 'components/FileIcon'
-import { Key } from 'components/Key'
-import { LoadingBar } from 'components/LoadingBar'
-import { MainInput } from 'components/MainInput'
-import { observer } from 'mobx-react-lite'
-import { FC, useEffect, useRef } from 'react'
-import { StyleSheet, Text, TouchableOpacity, View, ViewStyle } from 'react-native'
-import { useStore } from 'store'
-import { PasteItem } from 'stores/clipboard.store'
+import { LegendList, type LegendListRef } from "@legendapp/list";
+import clsx from "clsx";
+import { FileIcon } from "components/FileIcon";
+import { Key } from "components/Key";
+import { LoadingBar } from "components/LoadingBar";
+import { MainInput } from "components/MainInput";
+import { observer } from "mobx-react-lite";
+import { type FC, useEffect, useRef } from "react";
+import {
+	StyleSheet,
+	Text,
+	TouchableOpacity,
+	View,
+	type ViewStyle,
+} from "react-native";
+import { useStore } from "store";
+import type { PasteItem } from "stores/clipboard.store";
 
 interface Props {
-  style?: ViewStyle
-  className?: string
+	style?: ViewStyle;
+	className?: string;
 }
 
 const RenderItem = observer(
-  ({ item, index }: { item: PasteItem; index: number }) => {
-    const store = useStore()
-    const selectedIndex = store.ui.selectedIndex
-    const isActive = index === selectedIndex
-    return (
-      <TouchableOpacity
-        onPress={() => {
-          store.ui.setSelectedIndex(index)
-          store.keystroke.simulateEnter()
-        }}
-        className={clsx('items-center flex-row rounded gap-2 p-2', {
-          'bg-accent': isActive,
-          'opacity-80': !isActive,
-        })}>
-        <FileIcon
-          url={decodeURIComponent(
-            item.bundle?.replace('file://', '') ??
-            item.url?.replace('file://', '') ??
-            '',
-          )}
-          className="h-6 w-6"
-        />
+	({ item, index }: { item: PasteItem; index: number }) => {
+		const store = useStore();
+		const selectedIndex = store.ui.selectedIndex;
+		const isActive = index === selectedIndex;
+		return (
+			<TouchableOpacity
+				onPress={() => {
+					store.ui.setSelectedIndex(index);
+					store.keystroke.simulateEnter();
+				}}
+				className={clsx("items-center flex-row rounded gap-2 p-2", {
+					"bg-accent": isActive,
+					"opacity-80": !isActive,
+				})}
+			>
+				<FileIcon
+					url={decodeURIComponent(
+						item.bundle?.replace("file://", "") ??
+							item.url?.replace("file://", "") ??
+							"",
+					)}
+					className="h-6 w-6"
+				/>
 
-        <Text className={clsx('text-xs text flex-1')} numberOfLines={1}>
-          {item.text.trim()}
-        </Text>
+				<Text
+					className={clsx("text-xs text flex-1", {
+						"text-white": isActive,
+					})}
+					numberOfLines={1}
+				>
+					{item.text.trim()}
+				</Text>
 
-        {/* {!!item.url && (
+				{/* {!!item.url && (
         <Image src={`file://${item.url}`} className="h-20 w-20" />
       )} */}
-      </TouchableOpacity>
-    )
-  },
-)
+			</TouchableOpacity>
+		);
+	},
+);
 
 function isPngOrJpg(url: string | null | undefined) {
-  if (!url) {
-    return false
-  }
-  let lowercaseUrl = url.toLowerCase()
-  return (
-    lowercaseUrl.includes('.png') ||
-    lowercaseUrl.includes('.jpg') ||
-    lowercaseUrl.includes('.jpeg')
-  )
+	if (!url) {
+		return false;
+	}
+	const lowercaseUrl = url.toLowerCase();
+	return (
+		lowercaseUrl.includes(".png") ||
+		lowercaseUrl.includes(".jpg") ||
+		lowercaseUrl.includes(".jpeg")
+	);
 }
 
 export const ClipboardWidget: FC<Props> = observer(() => {
-  const store = useStore()
-  const data = store.clipboard.clipboardItems
-  const selectedIndex = store.ui.selectedIndex
-  const listRef = useRef<LegendListRef | null>(null)
+	const store = useStore();
+	const data = store.clipboard.clipboardItems;
+	const selectedIndex = store.ui.selectedIndex;
+	const listRef = useRef<LegendListRef | null>(null);
 
-  useEffect(() => {
-    if (data.length > 0 && selectedIndex < data.length) {
-      listRef.current?.scrollToIndex({
-        index: store.ui.selectedIndex,
-        viewOffset: 80,
-      })
-    }
-  }, [selectedIndex])
+	useEffect(() => {
+		if (data.length > 0 && selectedIndex < data.length) {
+			listRef.current?.scrollToIndex({
+				index: store.ui.selectedIndex,
+				viewOffset: 80,
+			});
+		}
+	}, [selectedIndex, data.length, store.ui.selectedIndex]);
 
-  return (
-    <View className="flex-1">
-      <View className="flex-row px-3">
-        <MainInput placeholder="Search Pasteboard..." showBackButton />
-      </View>
-      <LoadingBar />
-      <View className="flex-1 flex-row">
-        <View className="w-64 h-full">
-          <LegendList
-            key={`${data.length}`}
-            data={data}
-            className='flex-1'
-            contentContainerStyle={STYLES.contentContainer}
-            ref={listRef}
-            recycleItems
-            ListEmptyComponent={
-              <View className="flex-1 justify-center items-center">
-                <Text className="darker-text">[ ]</Text>
-              </View>
-            }
-            renderItem={RenderItem}
-          />
-        </View>
-        <View className="flex-1 px-3 py-2">
-          {!!data[selectedIndex] && (
-            <View className="dark:bg-black/20 bg-white rounded-lg p-3">
-              {!data[selectedIndex].url && (
-                <Text className="text-xs">
-                  {data[selectedIndex]?.text ?? []}
-                </Text>
-              )}
-              {/* {!!data[selectedIndex].url &&
+	return (
+		<View className="flex-1">
+			<View className="flex-row px-3">
+				<MainInput placeholder="Search Pasteboard..." showBackButton />
+			</View>
+			<LoadingBar />
+			<View className="flex-1 flex-row">
+				<View className="w-64 h-full">
+					<LegendList
+						key={`${data.length}`}
+						data={data}
+						className="flex-1"
+						contentContainerStyle={STYLES.contentContainer}
+						ref={listRef}
+						recycleItems
+						ListEmptyComponent={
+							<View className="flex-1 justify-center items-center">
+								<Text className="darker-text">[ ]</Text>
+							</View>
+						}
+						renderItem={RenderItem}
+					/>
+				</View>
+				<View className="flex-1 px-3 py-2">
+					{!!data[selectedIndex] && (
+						<View className="dark:bg-black/20 bg-white rounded-lg p-3">
+							{!data[selectedIndex].url && (
+								<Text className="text-xs">
+									{data[selectedIndex]?.text ?? []}
+								</Text>
+							)}
+							{/* {!!data[selectedIndex].url &&
               isPngOrJpg(data[selectedIndex].url) && (
                 <Image
                   source={{
@@ -119,43 +131,39 @@ export const ClipboardWidget: FC<Props> = observer(() => {
                   style={{resizeMode: 'contain'}}
                 />
               )} */}
-              {!!data[selectedIndex].url &&
-                !isPngOrJpg(data[selectedIndex].url) && (
-                  <View className="flex-1 w-full items-center justify-center">
-                    <FileIcon
-                      url={`file://${data[selectedIndex].url}`}
-                      className="h-20 w-20"
-                    />
-                    <Text className='font-mono'>{data[selectedIndex].text}</Text>
-                  </View>
-                )}
-            </View>
-          )}
-        </View>
-      </View>
-      {/* Shortcut bar at the bottom */}
-      <View className="py-2 px-4 flex-row items-center justify-end gap-1 subBg border-t border-color">
-        <Text className="text-xs darker-text mr-1">Delete All</Text>
-        <Key symbol={'⇧'} />
-        <Key symbol={'⌫'} />
-        <View className="mx-2" />
-        <Text className="text-xs darker-text mr-1">Delete Item</Text>
-        <Key symbol={'⌫'} />
-        <View className="mx-2" />
-        <Text
-          className={'text-xs mr-1'}>
-          Paste
-        </Text>
-        <Key symbol={'⏎'} primary />
-      </View>
-    </View>
-  )
-})
+							{!!data[selectedIndex].url &&
+								!isPngOrJpg(data[selectedIndex].url) && (
+									<View className="flex-1 w-full items-center justify-center">
+										<FileIcon
+											url={`file://${data[selectedIndex].url}`}
+											className="h-20 w-20"
+										/>
+										<Text className="font-mono">
+											{data[selectedIndex].text}
+										</Text>
+									</View>
+								)}
+						</View>
+					)}
+				</View>
+			</View>
+			{/* Shortcut bar at the bottom */}
+			<View className="py-2 px-4 flex-row items-center justify-end gap-1 subBg border-t border-color">
+				<Text className="text-xs darker-text mr-1">Delete Item</Text>
+				<Key symbol={"⇧"} />
+				<Key symbol={"⌫"} />
+				<View className="mx-2" />
+				<Text className={"text-xs mr-1"}>Paste</Text>
+				<Key symbol={"⏎"} primary />
+			</View>
+		</View>
+	);
+});
 
 const STYLES = StyleSheet.create({
-  contentContainer: {
-    flexGrow: 1,
-    paddingVertical: 6,
-    paddingLeft: 8,
-  },
-})
+	contentContainer: {
+		flexGrow: 1,
+		paddingVertical: 6,
+		paddingLeft: 8,
+	},
+});
