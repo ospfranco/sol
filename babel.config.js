47ebbf82b4d0c45692013ebc1205a38637083bfb
Oscar Franco <ospfranco@gmail.com>
2026-04-08 09:41:44 -0400

    various fixes

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset', 'nativewind/babel'],
   plugins: [
+    '@babel/plugin-transform-export-namespace-from',
     [
       'module-resolver',
       {
